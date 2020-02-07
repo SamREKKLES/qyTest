827e219e55732b598b1c0bcc981712b01b3c0c94
一路一年 <393707734@qq.com>
2020-02-07 19:21:03 +0800

    flask转换v1.0

--- qytest-run/src/main/java/zju/edu/qytest/controller/PatientsController.java
+++ qytest-run/src/main/java/zju/edu/qytest/controller/PatientsController.java
@@ -1,4 +1,11 @@
 package zju.edu.qytest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import zju.edu.qytest.service.PatientsService;
+
 public class PatientsController {
+
+    @Autowired
+    private PatientsService patientsService;
+
 }
